8d34f1a4a09a4267b21e0a9d1e28d7e1ff761045
kilokomuli <kilokoemma175@gmail.com>
2024-06-04 17:30:53 +0530

    updated

--- src/app/ui/footer.tsx
+++ src/app/ui/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer () {
     return (
         <footer>
-            <div className="flex justify-center items-center py-6 bg-gray-800 text-white">
+            <div className="flex justify-center items-center py-6 bg-gray-700 text-white bg-opacity-100">
                 <p className="text-lg font-bold">&copy; {new Date().getFullYear()} DevTales. All rights reserved.</p>
             </div>
         </footer>
