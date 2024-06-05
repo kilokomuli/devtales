e07930d8922c0340740b16552e0959a9586d26a2
kilokomuli <kilokoemma175@gmail.com>
2024-06-05 13:47:22 +0300

    updated about.tsx

--- src/app/ui/about.tsx
+++ src/app/ui/about.tsx
@@ -3,9 +3,9 @@ export default function About () {
         <section id="about" className="py-20 bg-violet-50 shadow-lg rounded-lg w-full">
             <div className="max-w-7xl mx-auto px-6">
                 <h2 className="text-4xl font-bold mb-8">About DevTales</h2>
-                <p className="text-lg mb-4">DevTales was inspired by our passion for web development and the desire to share our journey with the community. This project started as a way to document our learnings, projects, and experiences in the tech world. It evolved into a comprehensive platform where developers can find valuable resources, tutorials, and insights into the latest trends in web development.</p>
+                <p className="text-lg mb-4">DevTales was inspired by my passion for web development and the desire to share my journey with the community. This project started as a way to document my learnings, projects, and experiences in the tech world. With more features added it will evolve into a comprehensive platform where developers can find valuable resources, tutorials, and insights into the latest trends in web development.</p>
                 <p className="text-lg mb-4">
-                Our journey began in ALXSE program, where we were challenged to create a portfolio project that showcases our skills and growth. This project is a testament to our dedication and love for coding. You can learn more about our school and its programs <a href="https://www.alxafrica.com/" className="text-blue-500 hover:underline">here</a>.
+                Our journey began in ALXSE program, where we were challenged to create a portfolio project that showcases our skills and growth. This project is a testament to my dedication and love for coding. You can learn more about our school and its programs <a href="https://www.alxafrica.com/" className="text-blue-500 hover:underline">here</a>.
                 </p>
                 <div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-4">
           <div className="flex flex-col mb-4 md:mb-0">
